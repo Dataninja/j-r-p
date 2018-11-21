e92ec36a02dd12b06248e5248196fb07f4011c30
gvdr <ippogrifo.zoppo@gmail.com>
2018-11-22 08:23:16 +1300

    better tuftian plot

--- R/GoldenLion.R
+++ R/GoldenLion.R
@@ -1,38 +1,23 @@
-
-
 # Caricamento librerie ----------------------------------------------------
 
 library(tidyverse)
+library(magrittr)
 library(rvest)
 
 
 # Acquisizione dati da tabella html ---------------------------------------
 
-win_df <- 'https://it.wikipedia.org/wiki/Leone_d%27oro_al_miglior_film' %>%
+pivot_df <- 'https://it.wikipedia.org/wiki/Leone_d%27oro_al_miglior_film' %>%
   read_html() %>% #parsing html
   html_node(".wikitable") %>% 
   html_table(fill=T) %>% 
   filter(!str_detect(string = Film, pattern = 'non [venne||fu]')) %>% #filtro per escludere anni senza premiazioni
-  rename(Paese=Nazione) 
-
-
-# Scrittura csv -----------------------------------------------------------
-
-write_csv(x = win_df, path = "R/Winners.csv")
-
-
-# Creazione pivot ---------------------------------------------------------
-
-pivot_df <- win_df %>% 
+  rename(Paese=Nazione) %T>% # Scrivi CSV e continua oltre
+  write_csv(path = "R/Winners.csv") %>%
   separate_rows(Paese, sep = "/\\s?") %>% #split righe con più paesi per "/" e spazio
   count(Paese, sort = T) %>% #Conteggio e sort
-  rename(Vincitori = n)  
-
-
-# Scrittura csv -----------------------------------------------------------
-
-write_csv(x = pivot_df, path = "R/Countries.csv")
-
+  rename(Vincitori = n) %T>%
+  write_csv(path = "R/Countries.csv")
 
 # Creazione grafico a barre -----------------------------------------------
 
@@ -42,11 +27,14 @@ g <- ggplot(data = pivot_df,
   labs(x= "Paese vincitore",
        y="Numero di vittorie",
        title="Vincitori per paese d'origine",
-       caption= "Fonte: Wikipedia")+
-  
-  coord_flip()
-
+       caption= "Fonte: Wikipedia") +
+  scale_y_continuous(breaks = pivot_df %$% Vincitori %>% unique) +
+  coord_flip() +
+  theme_minimal() +
+  theme(panel.grid.major.x = element_line(linetype = "dotted", colour = "darkgrey"),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
 
 # Salvataggio grafico in png ----------------------------------------------
 
-ggsave(filename = "R/plot.png", device = "png")
+ggsave(plot = g, filename = "R/plot.png", device = "png")
