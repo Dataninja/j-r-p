ee34cdfc010aaba491f1f43e43f4adf089ab7d06
Andrea Zedda <a.zedda@kode-solutions.net>
2018-11-24 18:32:37 +0100

    Sbriglio un po' il pipe per renderlo più leggibile a neofiti

--- R/GoldenLion.R
+++ R/GoldenLion.R
@@ -1,40 +1,47 @@
 # Caricamento librerie ----------------------------------------------------
 
 library(tidyverse)
-library(magrittr)
 library(rvest)
-
 
 # Acquisizione dati da tabella html ---------------------------------------
 
-pivot_df <- 'https://it.wikipedia.org/wiki/Leone_d%27oro_al_miglior_film' %>%
+table_df <- 'https://it.wikipedia.org/wiki/Leone_d%27oro_al_miglior_film' %>%
   read_html() %>% #parsing html
-  html_node(".wikitable") %>% 
-  html_table(fill=T) %>% 
+  html_node(".wikitable") %>% #selezione del nodo 'wikitable'
+  html_table(fill=T) %>% #trasformazione in formato tabulare e gestione fill missing values
   filter(!str_detect(string = Film, pattern = 'non [venne||fu]')) %>% #filtro per escludere anni senza premiazioni
-  rename(Paese=Nazione) %T>% # Scrivi CSV e continua oltre
-  write_csv(path = "R/Winners.csv") %>%
-  separate_rows(Paese, sep = "/\\s?") %>% #split righe con più paesi per "/" e spazio
-  count(Paese, sort = T) %>% #Conteggio e sort
-  rename(Vincitori = n) %T>%
-  write_csv(path = "R/Countries.csv")
+  rename(Paese=Nazione) #to fix in it.wikipedia too
+
+#Scrivi CSV
+write_csv(x = table_df, path = "R/Winners.csv") 
+
+
+#Creazione pivot ----------------------------------------------------------------
+
+pivot_df <-  table_df %>% 
+             separate_rows(Paese, sep = "/\\s?") %>% #split righe con più paesi per "/" e spazio (con regex)
+             count(Paese, sort = T) %>% #Conteggio e sort
+             rename(Vincitori = n)
+  
+#Scrivi CSV
+write_csv(x = pivot_df, path = "R/Countries.csv")
 
 # Creazione grafico a barre -----------------------------------------------
 
-g <- ggplot(data = pivot_df,
+chart <- ggplot(data = pivot_df,
             aes(x = reorder(Paese,Vincitori), y = Vincitori)) +
-  geom_bar(stat='identity') +
-  labs(x= "Paese vincitore",
-       y="Numero di vittorie",
-       title="Vincitori per paese d'origine",
-       caption= "Fonte: Wikipedia") +
-  scale_y_continuous(breaks = pivot_df %$% Vincitori %>% unique) +
-  coord_flip() +
-  theme_minimal() +
-  theme(panel.grid.major.x = element_line(linetype = "dotted", colour = "darkgrey"),
-        panel.grid.major.y = element_blank(),
-        panel.grid.minor = element_blank())
+         geom_bar(stat='identity') +
+            labs(x= "Paese vincitore",
+                 y="Numero di vittorie",
+                 title="Vincitori per paese d'origine",
+                 caption= "Fonte: Wikipedia") + #da qui sono modifiche estetiche
+         scale_y_continuous(breaks = pivot_df %$% Vincitori %>% unique) + #personalizzazione scala
+         coord_flip() +
+         theme_minimal() + #da qua personalizzazione elementi tema grafico
+         theme(panel.grid.major.x = element_line(linetype = "dotted", colour = "darkgrey"),
+           panel.grid.major.y = element_blank(),
+           panel.grid.minor = element_blank())
 
 # Salvataggio grafico in png ----------------------------------------------
 
-ggsave(plot = g, filename = "R/plot.png", device = "png")
+ggsave(plot = chart, filename = "R/plot.png", device = "png")
