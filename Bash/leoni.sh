47a75d14d0ea768342133e580071f68d88c3d016
aborruso <aborruso@gmail.com>
2018-12-02 15:55:49 +0100

    Creata copia superlight dello script bash

--- Bash/leoni.sh
+++ Bash/leoni.sh
@@ -1,37 +1,31 @@
  #!/bin/bash
+
+ ### requisiti ###
+ # htmltable2flatgrid https://github.com/aborruso/htmltable2flatgrid/blob/master/htmltable2flatgrid.py
+ # miller http://johnkerl.org/miller/doc/
+ ### requisiti ###
  
- # accedi alla pagina
-curl "https://it.wikipedia.org/wiki/Leone_d%27oro_al_miglior_film" |
-	# filtra soltantto i record della tabella con i leoni e estraila in JSON
-	pup --charset utf-8 -p 'div.mw-parser-output > table.wikitable > tbody > tr:not(:first-child) json{}' >./raw.json
-# estrai anni
-jq <./raw.json '[.[].children | select(.|length > 2)] | {anno:.[][0].children[0].text}' |
-	mlr --j2c cat >./anni.csv
-# estrai titoli per anni con un solo vincitore
-jq <./raw.json '[.[].children | select(.|length > 2)] | {titoloUno:.[][1].children[0].children[0].text}' |
-	mlr --j2c cat >./titolo1.csv
-# estrai titoli per anni con più di un vincitore
-jq <./raw.json '[.[].children | select(.|length > 2)] | {titoloDue:.[][0].children[0].children[0].text}' |
-	mlr --j2c cat >./titolo2.csv
-# estrai regista per anni con un solo vincitore
-jq <./raw.json '[.[].children | select(.|length > 2)] | {registaUno:.[][2].children[0].text}' |
-	mlr --j2c cat >./regista1.csv
-# estrai regista per anni con più di un vincitore
-jq <./raw.json '[.[].children | select(.|length > 2)] | {registaDue:.[][1].children[0].text}' |
-	mlr --j2c cat >./regista2.csv
-# fai il merge dei dati estratti
-paste -d "," ./anni.csv ./titolo1.csv ./titolo2.csv ./regista1.csv ./regista2.csv >./anniTitoloRegista.csv
-# normalizza i dati
-mlr --csv fill-down -f anno then put 'if ($titoloDue != "") {$titoloUno = $titoloDue}' \
-	then put 'if ($registaDue != "") {$registaUno = $registaDue}' then cut -f anno,titoloUno,registaUno \
-	then rename titoloUno,titolo,registaUno,regista ./anniTitoloRegista.csv >./leoni_tmp.csv
-# estrai i dati sulle nazioni vincitrici
-jq <./raw.json '[.[].children | select(.|length > 2)] | .[] | if (.|length) > 3 then 
-([{nazione:.[3].children[].text }] | map(select(.nazione!= null).nazione) | . | 
-{nazione:join(":")}) else  ([{nazione:.[2].children[].text }] | 
-map(select(.nazione!= null).nazione) | . | {nazione:join(":")})  end' | mlr --j2c cat >./nazioni.csv
-# aggiungi i dati sulle nazioni ai dati già estratti
-paste -d "," ./leoni_tmp.csv ./nazioni.csv >./leoni.csv
-# fai il conteggio per nazione
-mlr --csv cut -o -f nazione then nest --explode --values --across-records -f nazione --nested-fs : \
-	then count-distinct -f nazione -o conteggio then sort -nr conteggio ./leoni.csv >./leoniNazione.csv
+ # estrai la terza tabella dalla pagina di wikipedia
+htmltable2flatgrid.py "https://it.wikipedia.org/wiki/Leone_d%27oro_al_miglior_film" 2
+
+
+# estrai soltanto gli anni con il premio assegnato
+# rimuovi righe con anni zenza vincitori
+# normalizza il separatore degli anni con più nazioni vincitrici
+
+tail -n +2 ./table.csv | \
+mlr --csv filter -S '$Regista =~ ".+"' \
+then fill-down -f Anno \
+then put '$Nazione=gsub($Nazione,"/ +","|")' >./leoni.csv
+
+# cancella la cartella raw di download
+rm ./table.csv
+
+# estrai la colonna Nazione
+# esplodi in verticale le celle, in presenza del separaratore | per gli anni con più nazioni premiate
+# conteggia per valori distinti di Nazione
+# ordina per Nazione
+mlr --csv cut -o -f Nazione \
+then nest --explode --values --across-records -f Nazione --nested-fs "|" \
+then count-distinct -f Nazione -o Conteggio \
+then sort -nr Conteggio leoni.csv >./leoniNazione.csv
---
+++ Bash/old_version/readme.md
@@ -0,0 +1,77 @@
+# Estrarre i Leoni d'oro da Wikipedia a riga di comando
+
+## requisiti
+
+- [curl](https://curl.haxx.se/);
+- [pup](https://github.com/ericchiang/pup);
+- [jq](https://stedolan.github.io/jq/);
+- [miller](https://github.com/johnkerl/miller).
+
+
+```bash
+# accedi alla pagina
+curl "https://it.wikipedia.org/wiki/Leone_d%27oro_al_miglior_film" |
+	# filtra soltantto i record della tabella con i leoni e estraila in JSON
+	pup --charset utf-8 -p 'div.mw-parser-output > table.wikitable > tbody > tr:not(:first-child) json{}' >./raw.json
+# estrai anni
+jq <./raw.json '[.[].children | select(.|length > 2)] | {anno:.[][0].children[0].text}' |
+	mlr --j2c cat >./anni.csv
+# estrai titoli per anni con un solo vincitore
+jq <./raw.json '[.[].children | select(.|length > 2)] | {titoloUno:.[][1].children[0].children[0].text}' |
+	mlr --j2c cat >./titolo1.csv
+# estrai titoli per anni con più di un vincitore
+jq <./raw.json '[.[].children | select(.|length > 2)] | {titoloDue:.[][0].children[0].children[0].text}' |
+	mlr --j2c cat >./titolo2.csv
+# estrai regista per anni con un solo vincitore
+jq <./raw.json '[.[].children | select(.|length > 2)] | {registaUno:.[][2].children[0].text}' |
+	mlr --j2c cat >./regista1.csv
+# estrai regista per anni con più di un vincitore
+jq <./raw.json '[.[].children | select(.|length > 2)] | {registaDue:.[][1].children[0].text}' |
+	mlr --j2c cat >./regista2.csv
+# fai il merge dei dati estratti
+paste -d "," ./anni.csv ./titolo1.csv ./titolo2.csv ./regista1.csv ./regista2.csv >./anniTitoloRegista.csv
+# normalizza i dati
+mlr --csv fill-down -f anno then put 'if ($titoloDue != "") {$titoloUno = $titoloDue}' \
+	then put 'if ($registaDue != "") {$registaUno = $registaDue}' then cut -f anno,titoloUno,registaUno \
+	then rename titoloUno,titolo,registaUno,regista ./anniTitoloRegista.csv >./leoni_tmp.csv
+# estrai i dati sulle nazioni vincitrici
+jq <./raw.json '[.[].children | select(.|length > 2)] | .[] | if (.|length) > 3 then 
+([{nazione:.[3].children[].text }] | map(select(.nazione!= null).nazione) | . | 
+{nazione:join(":")}) else  ([{nazione:.[2].children[].text }] | 
+map(select(.nazione!= null).nazione) | . | {nazione:join(":")})  end' | mlr --j2c cat >./nazioni.csv
+# aggiungi i dati sulle nazioni ai dati già estratti
+paste -d "," ./leoni_tmp.csv ./nazioni.csv >./leoni.csv
+# fai il conteggio per nazione
+mlr --csv cut -o -f nazione then nest --explode --values --across-records -f nazione --nested-fs : \
+	then count-distinct -f nazione -o conteggio then sort -nr conteggio ./leoni.csv >./leoniNazione.csv
+```
+
+## output
+
+| nazione | conteggio |
+| --- | --- |
+| Francia | 11 |
+| Italia | 11 |
+| Stati Uniti | 9 |
+| Regno Unito | 4 |
+| Cina | 4 |
+| Giappone | 3 |
+| Germania Ovest | 3 |
+| Taiwan | 3 |
+| India | 2 |
+| URSS | 2 |
+| Polonia | 2 |
+| Russia | 2 |
+| Cecoslovacchia | 1 |
+| Danimarca | 1 |
+| Algeria | 1 |
+| Macedonia | 1 |
+| Vietnam | 1 |
+| Iran | 1 |
+| Irlanda | 1 |
+| Israele | 1 |
+| Corea del Sud | 1 |
+| Svezia | 1 |
+| Venezuela | 1 |
+| Filippine | 1 |
+| Messico | 1 |
